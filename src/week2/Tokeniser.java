30f5d3f2746028e5f41172bea40929f6bdb26af7
Luiz Felix <lzcfelix@gmail.com>
2015-08-05 20:51:56 +1000

    Major improvments on week2 tasks

--- src/week2/Tokeniser.java
+++ src/week2/Tokeniser.java
@@ -16,11 +16,8 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 
 public class Tokeniser {
-	private final static String NED_TOKENS_FOLDER = "NED_tokens";
-	private final static String REGULAR_TOKENS_FOLDER = "_tokens";
-	
 	/* Path to the NER classifier file */
-	private static String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
+	private static String SERIALIZED_CLASSIFIER = "classifiers/english.all.3class.distsim.crf.ser.gz";
 	
 	/**
 	 * Reads every line of a file, removing HTML tags, multiple spaces and numbers if <code>allowNumbers</code>
@@ -63,92 +60,78 @@ public class Tokeniser {
 	}
 	
 	/**
-	 * Reads all the files from a folder and filter them using <code>purgeFile</code>. Ensure that all the
-	 * files on this folder are text. Each word will be replaced by /word[_annotation]/, where [_annontation] can
-	 * be PERSON, ORGANIZATION, LOCATION or not exist if the word doesn't fit in any of these classes. 
-	 * @param folderPath Path to the folder to be read.
-	 * @throws IOException If there's a fault while manipulating the file.
+	 * For each file on <code>corpusPath</code>, the following procedure is done:
+	 * 1. For each file, a corresponding one, with the same name, is created on a folder on <code>outputFolder</code>. 
+	 * If this folder doesn't exist, then it is created;
+	 * 2. For each file on <code>corpusPath</code> folder, HTML tags, extra spaces and numbers <code>allowNumbers</code> 
+	 * is set to <code>false</code> are removed;
+	 * 3. Stanford's NER Classifiers attempts to label each word on one of the categories: Person, Location or Organization. 
+	 * If it succeeds, the words is annotated using the format [word]_[class], otherwise it is left as it is;
+	 * 4. The word, either classified or not is written on a new line on the file on <code>corpusPath</code>; 
+	 * 
+	 * <pre>All the files on the source folder MUST be text files.</pre>
+	 * <post>If <code>outputFolder</code> doesn't exist, it is created during this function execution.</post> 
+	 * @param corpusPath The path to the corpus location.
+	 * @param outputFolder The path, including the folder name, to the directory that will hold the annotated files.
+	 * @throws IOException If there is an IO fault.
+	 * @throws ClassCastException NER thrown exception.
+	 * @throws ClassNotFoundException NER thrown exception.
 	 */
-	public static void TokeniseCollection(String folderPath) throws IOException {
-		File tokensFolder = new File(folderPath + REGULAR_TOKENS_FOLDER);
-		tokensFolder.mkdir();
+	public static void applyNEDClassifier(String corpusPath, String outputFolder, boolean allowNumbers) 
+			throws IOException, ClassCastException, ClassNotFoundException {
 		
-		Files.list(new File(folderPath).toPath()).forEach(path -> {			
-			PrintWriter fileWriter = null;
-			String purgedFile = "";
-			String tokensFile = path.getFileName().toString().substring(0, path.getFileName().toString().lastIndexOf('.')) + ".txt";
-			
-			try {
-				fileWriter = new PrintWriter(Paths.get(tokensFolder.getPath(), tokensFile).toString());
-				purgedFile = purgeFile(path, false);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-			
-			fileWriter.write(purgedFile);
-			fileWriter.flush();
-			fileWriter.close();
-		});
-	}
-	
-	/**
-	 * Walks though a folder of files containing tokens words and classify them using NED-Stanford classifier.
-	 * @param folderPath Path to the folder with the token files.
-	 * @throws ClassCastException Internal NED Classifier thrown exceptions.
-	 * @throws ClassNotFoundException Internal NED Classifier thrown exceptions.
-	 * @throws IOException If there's a fault while manipulating the file.
-	 */
-	public static void applyNED(String folderPath) throws ClassCastException, ClassNotFoundException, IOException {
 		//Loading the classifier
-		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(SERIALIZED_CLASSIFIER);
 		
-		//create a folder named NED_tokens on the upper level on the folders hierarchy 
-		File currentFolder = new File(folderPath);
+		//Creating the destination folder, if it doesn't exist
+		File of = new File(outputFolder);
+		of.mkdir();
 		
-		String parentFolder = currentFolder.getCanonicalPath().substring(0, currentFolder.getCanonicalPath().lastIndexOf('/'));
-		File tokensFolder = new File(Paths.get(parentFolder, NED_TOKENS_FOLDER).toString());
-		tokensFolder.mkdirs();
-		
-		//parse and annotate each file
-		Files.list(new File(folderPath).toPath()).forEach(path -> {
-			String filePath = Paths.get(tokensFolder.getPath(), path.getFileName().toString()).toString();
+		// Annotating each file
+		Files.list(new File(corpusPath).toPath()).forEach(path -> {
+			String purgedFile = "";
 			
 			try {
-				String fileContents = IOUtils.slurpFile(path.toString());
-				List<List<CoreLabel>> out = classifier.classify(fileContents);
-
-				PrintWriter pw = new PrintWriter(filePath);
-				
-				//I'd rather keep everything in memory to minimise the amount of disk accesses
-				StringBuilder buffer = new StringBuilder();
-				
-				for (List<CoreLabel> sentence : out)
-					for (CoreLabel word : sentence) {
-						buffer.append(word.word());
-						
-						if (word.get(CoreAnnotations.AnswerAnnotation.class) != null)
-							buffer.append('_' + word.get(CoreAnnotations.AnswerAnnotation.class));
-							
-						buffer.append('\n');
-					}
-				
-				pw.write(buffer.toString());
-				pw.flush();
-				pw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+				purgedFile = purgeFile(path, allowNumbers);
 			}
+			catch (IOException e) {
+				System.err.println("Error while parsing file " + path);
+			}
+			
+			//apply the classification
+			List<List<CoreLabel>> out = classifier.classify(purgedFile);
+			
+			//I'd rather keep everything in memory to minimise disk access
+			StringBuilder buffer = new StringBuilder();
+			for (List<CoreLabel> sentence : out)
+				for (CoreLabel word : sentence) {
+					buffer.append(word.word());
+					
+					if (word.get(CoreAnnotations.AnswerAnnotation.class) != null)
+						buffer.append('_' + word.get(CoreAnnotations.AnswerAnnotation.class));
+						
+					buffer.append('\n');
+				}
+			
+			
+			String newFile = Paths.get(outputFolder, path.getFileName().toString()).toString();
+			
+			// writing annotated file to disk 
+			try {
+				PrintWriter output = new PrintWriter(newFile);
+				output.write(buffer.toString());
+				output.flush();
+				output.close();
+			} catch (Exception e) { }
 		});
 		
+		
 	}
-	
+		
 	/* Didn't want to write a new class just to tokenise the files */
 	public static void main(String args[]) {		
 		try {
-			System.out.println("Tokenising...");
-			TokeniseCollection("corpus");
-			System.out.println("Labelling...");
-			applyNED("corpus" + REGULAR_TOKENS_FOLDER);
+			applyNEDClassifier("corpus", "korpus", false);
 			System.out.println("Done.");
 		} catch (Exception e) {
 			e.printStackTrace();
