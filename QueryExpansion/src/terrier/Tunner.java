b1b5d321d426f1200d8db514234a0d50206b433c
Luiz Felix <lzcfelix@gmail.com>
2015-10-18 19:22:20 +1000

    Implemented mu-tunning (I think so).

---
+++ QueryExpansion/src/terrier/Tunner.java
@@ -0,0 +1,116 @@
+package terrier;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Map.Entry;
+
+import org.terrier.structures.CollectionStatistics;
+import org.terrier.structures.DocumentIndex;
+import org.terrier.structures.DocumentIndexEntry;
+import org.terrier.structures.Index;
+import org.terrier.structures.Lexicon;
+import org.terrier.structures.LexiconEntry;
+import org.terrier.structures.PostingIndex;
+import org.terrier.structures.bit.InvertedIndex;
+import org.terrier.structures.postings.IterablePosting;
+
+public class Tunner {
+	// the maximum of iterations performed by Newton's Method
+	public final int MAX_ITERATIONS = 40000;
+	
+	// the minimum precision on the derivative to consider the maxima point.
+	public final double EPSILON = 1e-4;
+	
+	private Index index;
+	private PostingIndex<?> invertedIndex;
+	private Lexicon<String> vocabulary;
+	
+	/**
+	 * Creates a Tunner object, which is able to fit mu and lambda according to
+	 * C Zhai and J. Lafferty Paper
+	 * 
+	 * @param index The terrier Index object
+	 */
+	public Tunner(Index index) {
+		this.index = index;
+	
+		invertedIndex = index.getInvertedIndex();
+		vocabulary = index.getLexicon();
+	}
+	
+	/**
+	 * The g function described on the paper. This implementation differs from what is on the paper in:
+	 *  - The loops were interchanged, as not all documents contains all words
+	 *  - The fraction powers were manipulated to achieve more efficience using power properties.
+	 * @param mu The initial mu value. According to the authors, 1 is a good starting point.
+	 * @param samples How much of the corpus is going to be considered while tuning.
+	 * @return g(mu)
+	 * @throws IOException If there's an I/O fault while reading from the disk.
+	 */
+	private double g(double mu, double samples) throws IOException {
+		long N = (long)Math.ceil(index.getDocumentIndex().getNumberOfDocuments() * samples);
+		int vocabularySize = (int)Math.ceil(index.getCollectionStatistics().getNumberOfUniqueTerms() * samples);
+		
+		CollectionStatistics statistics = index.getCollectionStatistics();
+		
+		double result = 0;
+		
+		// loops are the inverse from the paper
+		
+		for (int w = 0; w < vocabularySize; w++) {
+			Entry<String, LexiconEntry> currentWord = vocabulary.getIthLexiconEntry(w);
+			
+			double p_w_c = currentWord.getValue().getNumberOfEntries() / (double)statistics.getNumberOfTokens(); 
+			double A, B;
+			
+			// this is the outer loop in the paper
+			IterablePosting postingsList = invertedIndex.getPostings(currentWord.getValue());
+			while (postingsList.next() != IterablePosting.EOL) {
+				
+				double c_w_d = postingsList.getFrequency();
+				double d_len = postingsList.getDocumentLength() - 1;	//the paper used doc_len - 1
+				
+				A = c_w_d * Math.pow((d_len * p_w_c - c_w_d + 1), 2);
+				B = Math.pow((d_len + mu) * (c_w_d - 1 + mu * p_w_c), 2);
+				
+				result += A / B;
+			}
+		}
+		
+		return result;
+	}
+	
+	// the formula can be resumed in:
+	// g  = A/(B*C)
+	// g' = -A^2/(B^2*C^2) = -A^2/(B*C)^2
+	// B*C = D --> g = A/D ; g' = -A^2/D^2 = -(A/D)^2 -> g' = -g^2
+	
+	/**
+	 * Tunes the mu parameter, used on Dirichlet or DJM Language Model based on the
+	 * corpus. This is done through Newton's method. Since the method always converges, according
+	 * to the authors (and it probably does as the convergence factor is a 1/p(mu) function, this
+	 * method returns mu if either: the modulus of the first derivative is smaller than <code>EPSILON</code>,
+	 * or the amount of iterations is bigger than <code>MAX_ITERATIONS</code>. Relative difference on
+	 * the update value is ignored.
+	 * @param mu The initial value of mu. According to the paper authors 1 is a good initial point.
+	 * @param samples The percentage of the corpus that is going to be taken in account when tunning mu.
+	 * This value must be ]0,1[.
+	 * @return The tuned value of Mu.
+	 * @throws IOException If there's an I/O fault while reading the index data.
+	 */
+	public double tuneMu(double mu, double samples) throws IOException {
+		double g;
+		int counter = 0;
+		
+		do {
+			g = g(mu, samples);
+			mu = mu + 1/g;
+			
+			System.out.println("g(mu) " + g);
+			System.out.println("Iteration " + counter + " mu = " + mu);
+		} while (++counter < MAX_ITERATIONS && Math.abs(g) > EPSILON);
+		
+		return mu;
+	}	
+
+}
